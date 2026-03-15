732211f046490f16c67f4d87c16fdc1fc65a18d3
PrincessFluffyButt937 <jhosch@seznam.cz>
2026-03-15 18:04:39 +0100

    CH2 L4 done

--- cmd.go
+++ cmd.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"os"
+	"time"
 
 	"github.com/PrincessFluffyButt937/Blog-Aggregator/internal/config"
+	"github.com/PrincessFluffyButt937/Blog-Aggregator/internal/database"
+	"github.com/google/uuid"
 )
 
 type state struct {
 	con *config.Config
+	db  *database.Queries
 }
 
 type command struct {
@@ -37,9 +44,47 @@ func (c *commands) register(name string, f func(*state, command) error) {
 
 func handlerLogin(s *state, c command) error {
 	if len(c.arg) == 0 {
-		return fmt.Errorf("handlerLogin error: No args")
+		fmt.Println("handlerLogin error: No args")
+		os.Exit(1)
+	}
+	_, err := s.db.GetUser(context.Background(), sql.NullString{String: c.arg[0], Valid: true})
+	if err == sql.ErrNoRows {
+		fmt.Printf("handlerLogin error: User %s does not exist!\n", c.arg[0])
+		os.Exit(1)
 	}
 	s.con.Current_user_name = c.arg[0]
 	fmt.Printf("User %s has been set.\n", c.arg[0])
+	return nil
+}
+
+func handlerRegister(s *state, c command) error {
+	if len(c.arg) == 0 {
+		fmt.Println("handlerLogin error: No args")
+		os.Exit(1)
+	}
+	user_data := database.CreateUserParams{
+		ID:        uuid.New(),
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+		Name:      sql.NullString{String: c.arg[0], Valid: true},
+	}
+	_, err := s.db.GetUser(context.Background(), sql.NullString{String: c.arg[0], Valid: true})
+	if err != sql.ErrNoRows {
+		fmt.Printf("handlerRegister error: User %s already exists!\n", c.arg[0])
+		os.Exit(1)
+	}
+	s.db.CreateUser(context.Background(), user_data)
+	s.con.Current_user_name = c.arg[0]
+	fmt.Printf("User %s was successfuly registered.\n", c.arg[0])
+	return nil
+}
+
+func handlerReset(s *state, c command) error {
+	err := s.db.DelUsers(context.Background())
+	if err != nil {
+		fmt.Printf("handlerReset error: %s\n", err)
+		os.Exit(1)
+	}
+	fmt.Println("Users table reset was successul.")
 	return nil
 }
--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"os"
 
 	"github.com/PrincessFluffyButt937/Blog-Aggregator/internal/config"
+	"github.com/PrincessFluffyButt937/Blog-Aggregator/internal/database"
 	_ "github.com/lib/pq"
 )
 
@@ -12,18 +14,33 @@ func main() {
 	args := os.Args
 	Cfg_path, _ := config.Get_cfg_path()
 	Cfg, _ := config.Read_cfg(Cfg_path)
+	db, err := sql.Open("postgres", Cfg.Db_url)
+	if err != nil {
+		fmt.Printf("Database connection error: %s\n", err)
+		os.Exit(1)
+		return
+	}
+	dbQuerries := database.New(db)
+
 	s := state{
 		con: Cfg,
+		db:  dbQuerries,
 	}
+
+	// Available commands start
 	coms := commands{
 		repo: make(map[string]func(*state, command) error),
 	}
 	coms.register("login", handlerLogin)
-	if len(args) < 3 {
-		fmt.Println("Error, # of Args is less than 2")
+	coms.register("register", handlerRegister)
+	coms.register("reset", handlerReset)
+	if len(args) < 2 {
+		fmt.Println("Error, # of Args is less than 1")
 		os.Exit(1)
 		return
 	}
+	// Available commands end
+
 	com := command{
 		name: args[1],
 		arg:  args[2:],
